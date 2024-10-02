c8ad03dfe33b17a64af933a26d58112bba721468
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-10-02 15:46:24 +0200

    Refactor App.jsx to use react-router-dom for navigation and implement ContactBookContext for managing contacts

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,46 @@
-import './App.css';
+import { Link, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { useContext, useEffect } from "react";
+import { ContactBookContext } from "./context";
+import ContactPage from "./components/contactPage/ContactPage";
+import ContactProfile from "./components/contactProfilePage/ContactProfile";
+import ContactForm from "./components/contactForm/ContactForm";
 
 function App() {
-    return (
-        <p>Hello, world!</p>
-    );
+  const { getContacts } = useContext(ContactBookContext);
+
+  useEffect(() => {
+    getContacts();
+  }, []);
+
+  return (
+    <div className="app-container">
+      <header>
+        <nav className="menu-left">
+          <div className="menu-left__header">
+            <h1>Menu</h1>
+          </div>
+          <div className="menu-left__content">
+            <ul>
+              <li>
+                <Link to="/contact">Contact List</Link>
+              </li>
+              <li>
+                <Link to="/add">Add New Contact</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<ContactPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/contact/:id" element={<ContactProfile />} />
+        <Route path="/add" element={<ContactForm />} />
+        <Route path="update/:id" element={<ContactForm />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
